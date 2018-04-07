5ae29e779eaaa4ade21e6c5ac4b3a5261a6fde9d
herbertdubon <hdubon@tecnasa.com>
2018-04-07 09:12:31 -0600

    Creando Archivo JSON

--- json/index.js
+++ json/index.js
@@ -1,1 +1,6 @@
-"nombre": "Jose Luis Ramirez Castro"
+console.log("iniciando");
+
+var Informacion = {"nombre":"Herbert"}
+var Datainfo = JSON.parse(Informacion);
+console.log("El nombre "+Informacion.nombre);
+console.log("El nombre "+Informacion["nombre"]);
